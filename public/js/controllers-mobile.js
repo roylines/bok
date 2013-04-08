42f5cdafb3d87f454c553716c0d608059da9084a
roylines <roy@roylines.co.uk>
2013-04-08 13:47:37 +0100

    added angularjs example

---
+++ public/js/controllers-mobile.js
@@ -0,0 +1,16 @@
+var ClassController = function($scope) {
+  $scope.classes = [
+    {name: 'A'},
+    {name: 'B'},
+    {name: 'C'},
+    {name: 'AA'},
+    {name: 'AB'},
+    {name: 'AC'},
+    {name: 'AAA'},
+    {name: 'ABA'},
+    {name: 'ACA'},
+    {name: 'AAA'},
+    {name: 'ABA'},
+    {name: 'ACA'},
+    {name: 'DA'}];
+};
